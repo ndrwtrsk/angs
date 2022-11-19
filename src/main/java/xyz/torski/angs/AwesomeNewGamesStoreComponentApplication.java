3720df90991c6f93b1137bb5fb35e5831f246be6
dev <dev@ambp.local>
2022-11-19 20:39:53 +0100

    init

---
+++ src/main/java/xyz/torski/angs/AwesomeNewGamesStoreComponentApplication.java
@@ -0,0 +1,13 @@
+package xyz.torski.angs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeNewGamesStoreComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeNewGamesStoreComponentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/torski/angs/AwesomeNewGamesStoreComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.torski.angs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeNewGamesStoreComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
